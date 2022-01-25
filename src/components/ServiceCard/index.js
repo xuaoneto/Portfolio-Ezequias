3350ae439231b7db88c1f001abd1ed923c0d25b4
João Fernandes Praxedes Neto <jofernandes47@gmail.com>
2022-01-24 21:09:50 -0300

    correction

--- src/components/ServiceCard/index.js
+++ src/components/ServiceCard/index.js
@@ -9,6 +9,7 @@ export function ServiceCard({ icon, children, ...rest }) {
       borderRadius="12px"
       h="100%"
       w="100%"
+      spacing="20px"
       p={{ base: "20px", lg: "35px" }}
       boxShadow="0 0 20px rgba(0,0,0, 0.2)"
       _hover={{ boxShadow: "0 0 20px rgba(0,0,0, 0.4)" }}
